c118d4855ad3aaa5c811fb9f4ef1b11415d116c0
GolovaDanyla <bussinestarget4you@gmail.com>
2022-09-28 11:10:45 +0300

    pre-release 0.0.2

--- 1190.js
+++
@@ -1,4 +0,0 @@
-"use strict";(self.webpackChunkapp=self.webpackChunkapp||[]).push([[1190],{1190:(P,n,s)=>{s.r(n),s.d(n,{startStatusTap:()=>i});var r=s(5861),o=s(7936),_=s(8935),d=s(3756);
-/*!
-     * (C) Ionic http://ionicframework.com - MIT License
-     */const i=()=>{const t=window;t.addEventListener("statusTap",()=>{(0,o.f)(()=>{const a=document.elementFromPoint(t.innerWidth/2,t.innerHeight/2);if(!a)return;const e=(0,_.f)(a);e&&new Promise(h=>(0,d.c)(e,h)).then(()=>{(0,o.c)((0,r.Z)(function*(){e.style.setProperty("--overflow","hidden"),yield(0,_.s)(e,300),e.style.removeProperty("--overflow")}))})})})}}}]);
--- README.md
+++
@@ -1,1 +0,0 @@
-# SocialMessanger.github.io
