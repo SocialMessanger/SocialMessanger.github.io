0ef5a648b561148fc27c5af393b893b40ebe39b9
GolovaDanyla <bussinestarget4you@gmail.com>
2022-09-30 19:38:13 +0300

    pre-release 0.0.7

--- 4376.06779a13708afb3f.js
+++
@@ -1,7 +0,0 @@
-"use strict";(self.webpackChunkapp=self.webpackChunkapp||[]).push([[4376],{4376:(E,a,r)=>{r.r(a),r.d(a,{ion_text:()=>u});var s=r(7936),l=r(1671),_=r(2854);
-/*!
-     * (C) Ionic http://ionicframework.com - MIT License
-     */const u=class{constructor(o){(0,s.r)(this,o)}render(){const o=(0,l.b)(this);return(0,s.h)(s.H,{class:(0,_.c)(this.color,{[o]:!0})},(0,s.h)("slot",null))}};u.style=":host(.ion-color){color:var(--ion-color-base)}"},2854:(E,a,r)=>{r.d(a,{c:()=>_,g:()=>u,h:()=>l,o:()=>d});var s=r(5861);
-/*!
-     * (C) Ionic http://ionicframework.com - MIT License
-     */const l=(t,n)=>null!==n.closest(t),_=(t,n)=>"string"==typeof t&&t.length>0?Object.assign({"ion-color":!0,[`ion-color-${t}`]:!0},n):n,u=t=>{const n={};return(t=>void 0!==t?(Array.isArray(t)?t:t.split(" ")).filter(e=>null!=e).map(e=>e.trim()).filter(e=>""!==e):[])(t).forEach(e=>n[e]=!0),n},o=/^[a-z][a-z0-9+\-.]*:/,d=function(){var t=(0,s.Z)(function*(n,e,f,h){if(null!=n&&"#"!==n[0]&&!o.test(n)){const i=document.querySelector("ion-router");if(i)return e?.preventDefault(),i.push(n,f,h)}return!1});return function(e,f,h,i){return t.apply(this,arguments)}}()}}]);
---
+++ 4376.39a6b66f8455f2b6.js
@@ -0,0 +1,7 @@
+"use strict";(self.webpackChunkapp=self.webpackChunkapp||[]).push([[4376],{4376:(E,u,r)=>{r.r(u),r.d(u,{ion_text:()=>l});var s=r(1308),_=r(9658),a=r(2854);
+/*!
+     * (C) Ionic http://ionicframework.com - MIT License
+     */const l=class{constructor(o){(0,s.r)(this,o)}render(){const o=(0,_.b)(this);return(0,s.h)(s.H,{class:(0,a.c)(this.color,{[o]:!0})},(0,s.h)("slot",null))}};l.style=":host(.ion-color){color:var(--ion-color-base)}"},2854:(E,u,r)=>{r.d(u,{c:()=>a,g:()=>l,h:()=>_,o:()=>d});var s=r(5861);
+/*!
+     * (C) Ionic http://ionicframework.com - MIT License
+     */const _=(t,n)=>null!==n.closest(t),a=(t,n)=>"string"==typeof t&&t.length>0?Object.assign({"ion-color":!0,[`ion-color-${t}`]:!0},n):n,l=t=>{const n={};return(t=>void 0!==t?(Array.isArray(t)?t:t.split(" ")).filter(e=>null!=e).map(e=>e.trim()).filter(e=>""!==e):[])(t).forEach(e=>n[e]=!0),n},o=/^[a-z][a-z0-9+\-.]*:/,d=function(){var t=(0,s.Z)(function*(n,e,f,h){if(null!=n&&"#"!==n[0]&&!o.test(n)){const i=document.querySelector("ion-router");if(i)return e?.preventDefault(),i.push(n,f,h)}return!1});return function(e,f,h,i){return t.apply(this,arguments)}}()}}]);
