16d679dd25179d5b6f9b97fc5ef3e1b798b6e5e3
GolovaDanyla <bussinestarget4you@gmail.com>
2022-10-21 18:58:25 +0300

    pre-release 0.0.27

---
+++ 5780.5b80923b917922a6.js
@@ -0,0 +1,4 @@
+"use strict";(self.webpackChunkapp=self.webpackChunkapp||[]).push([[5780],{179:(h,n,s)=>{s.r(n),s.d(n,{startStatusTap:()=>i});var r=s(5861),o=s(1308),_=s(6642),d=s(5730);
+/*!
+     * (C) Ionic http://ionicframework.com - MIT License
+     */const i=()=>{const t=window;t.addEventListener("statusTap",()=>{(0,o.f)(()=>{const a=document.elementFromPoint(t.innerWidth/2,t.innerHeight/2);if(!a)return;const e=(0,_.f)(a);e&&new Promise(c=>(0,d.c)(e,c)).then(()=>{(0,o.c)((0,r.Z)(function*(){e.style.setProperty("--overflow","hidden"),yield(0,_.s)(e,300),e.style.removeProperty("--overflow")}))})})})}}}]);
